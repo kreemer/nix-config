0d6ac494b9aaaa20ab5cf6fdf65cda63c4dd9819
kreemer <kevin@familie-studer.ch>
2026-08-29 21:12:39 +0200

    feat: exclude some cosmic default apps

--- hosts/ideapad.nix
+++ hosts/ideapad.nix
@@ -5,10 +5,10 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./ideapad-hardware.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./ideapad-hardware.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -52,18 +52,25 @@
   users.users."kreemer" = {
     isNormalUser = true;
     description = "Kevin Studer";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [ ];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  environment.cosmic.excludePackages = with pkgs; [
+    cosmic-edit
+    cosmic-term
+  ]
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
