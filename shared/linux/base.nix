1301d7101a9e22770c0992492204afb66d82c522
kreemer <kevin@familie-studer.ch>
2026-08-29 21:31:07 +0200

    feat: add work related apps

--- shared/linux/base.nix
+++ shared/linux/base.nix
@@ -10,4 +10,10 @@
   nix.settings.experimental-features = "nix-command flakes";
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    teams-for-linux
+    docker
+    thunderbird
+  ];
 }
