275e30c58d7e224685287987d9873cc90e1833c1
kreemer <kevin@familie-studer.ch>
2026-09-03 09:10:34 +0200

    feat: add path to dynamic homebrew bin

--- system/darwin/base.nix
+++ system/darwin/base.nix
@@ -11,6 +11,11 @@
   system.configurationRevision = self.rev or self.dirtyRev or null;
   nixpkgs.hostPlatform = "aarch64-darwin";
 
+  environment.systemPath = [
+    "${config.homebrew.prefix}/bin"
+    "${config.homebrew.prefix}/sbin"
+  ];
+
   environment.systemPackages = with pkgs; [
     colima
     docker
