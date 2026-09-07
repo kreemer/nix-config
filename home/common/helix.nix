28255579b1f78b8b3be36bd8f620ec31acaf3a28
kreemer <kevin@familie-studer.ch>
2026-09-07 14:59:42 +0200

    feat: correct config for helix

--- home/common/helix.nix
+++ home/common/helix.nix
@@ -3,20 +3,64 @@
   programs.helix = {
     enable = true;
     settings = {
-      theme = "gruvbox_dark_hard";
-      editor.cursor-shape = {
-        normal = "block";
-        insert = "bar";
-        select = "underline";
+      theme = "monokai_pro_machine";
+      mouse = false;
+      true-color = true;
+      editor = {
+        soft-wrap = {
+          enable = true;
+        };
+      };
+      keys = {
+        insert = {
+          up = "no_op";
+          down = "no_op";
+          left = "no_op";
+          right = "no_op";
+        };
+        normal = {
+          C-j = [
+            "extend_to_line_bounds"
+            "delete_selection"
+            "paste_after"
+          ];
+          C-k = [
+            "extend_to_line_bounds"
+            "delete_selection"
+            "move_line_up"
+            "paste_before"
+          ];
+          up = "no_op";
+          down = "no_op";
+          left = "no_op";
+          right = "no_op";
+        };
       };
     };
-    languages.language = [
-      {
-        name = "nix";
-        auto-format = true;
-        formatter.command = lib.getExe pkgs.nixfmt;
-      }
-    ];
+
+    languages = {
+      language-server.rust-analyzer = {
+        command = "rust-analyzer";
+        config = {
+          rustc = {
+            source = "discover";
+          };
+        };
+      };
+
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+          formatter.command = lib.getExe pkgs.nixfmt;
+        }
+        {
+          name = "rust";
+          language-servers = [ "rust-analyzer" ];
+        }
+      ];
+    };
+
     themes = {
       autumn_night_transparent = {
         "inherits" = "autumn_night";
