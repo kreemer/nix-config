8c6ca35f72f6a1bf88af27156189f4fc7240b5f0
kreemer <kevin@familie-studer.ch>
2026-09-11 08:44:58 +0200

    feat: disable autosuspend for usb

--- hosts/ideapad.nix
+++ hosts/ideapad.nix
@@ -14,6 +14,13 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Load Intel Driver on boot
+  boot.initrd.kernelModules = [ "i915" ];
+
+  powerManagement.tlp.settings = {
+    USB_AUTOSUSPEND = 0;
+  };
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
