9bed5b165e0f5c9e82d7d913bfc87e6827d20212
kreemer <kevin@familie-studer.ch>
2026-09-11 13:03:24 +0200

    feat: implement impermanence within the system

--- flake.nix
+++ flake.nix
@@ -9,6 +9,12 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
     # Patched libfprint fork with support for the Goodix GF3268 (27c6:55b4)
     # fingerprint reader found in the Lenovo IdeaPad C340. Not a flake; consumed
     # as a source tree by the fingerprint overlay in system/linux/fingerprint.nix.
@@ -25,6 +31,7 @@
       nix-darwin,
       nixpkgs,
       home-manager,
+      impermanence,
       libfprint-goodix55b4,
     }:
     let
@@ -74,6 +81,7 @@
           commonModules
           ++ linuxOnlyModules
           ++ [
+            impermanence.nixosModules.impermanence
             ./hosts/ideapad
             home-manager.nixosModules.home-manager
             {
---
+++ home/linux/impermanence.nix
@@ -0,0 +1,20 @@
+{ config, ... }:
+{
+  home.persistence."/persist/home/${config.home.username}" = {
+    allowOther = true;
+    directories = [
+      "Documents"
+      "Downloads"
+      "Pictures"
+      "Videos"
+      ".config/cosmic"
+      ".config/git"
+      ".config/gh"
+      ".mozilla"
+      ".local/share/direnv"
+      ".local/share/keyrings"
+      ".gnupg"
+      ".ssh"
+    ];
+  };
+}
